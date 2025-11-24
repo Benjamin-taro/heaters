6ea9c3277494a846dc8c079680066c7b6dfc61ae
Yuki Ise <70761052+Benjamin-taro@users.noreply.github.com>
2025-11-24 17:16:39 +0000

    frontend from scratch

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideZoneChangeDetection } from '@angular/core';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideClientHydration(),
-    provideHttpClient(withFetch()),
-    provideRouter(routes),
-    provideAnimations(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
